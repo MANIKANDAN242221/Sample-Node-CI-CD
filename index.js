d0ff8ff54d8fdd10492afa03121981836204e2e8
Manikandan G <158708882+MANIKANDAN242221@users.noreply.github.com>
2025-08-11 15:13:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 9000;
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello World from Docker and Node.js!</h1>');
 });
-app.listen(port, () => {
+
+// Listen on all interfaces to allow external access
+app.listen(port, '0.0.0.0', () => {
     console.log(`Listening on port ${port}`);
 });
